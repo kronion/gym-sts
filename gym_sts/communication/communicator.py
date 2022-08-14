31af232bcabba3f95747ed82cf9d11b24cbba811
Zeus Kronion <kronion@users.noreply.github.com>
2022-08-14 15:12:10 -0500

    Actually check in the Communicator

---
+++ gym_sts/communication/communicator.py
@@ -0,0 +1,79 @@
+import os
+from pathlib import Path
+
+from gym_sts.communication.receiver import Receiver
+from gym_sts.communication.sender import Sender
+from gym_sts.envs.observations import Observation
+
+
+def init_fifos(filenames):
+    # Create fifos for communication
+    for f in filenames:
+        if os.path.exists(f):
+            os.remove(f)
+        os.mkfifo(f)
+
+
+class Communicator:
+    def __init__(self, input_path: Path, output_path: Path):
+        self.input_path = input_path
+        self.output_path = output_path
+        init_fifos([self.input_path, self.output_path])
+        self.receiver = Receiver(self.output_path)
+        self.sender = Sender(self.input_path)
+
+    def ready(self) -> None:
+        self.sender.send_ready()
+
+    def choose(self, choice) -> Observation:
+        # self.receiver.empty_fifo()
+        self.sender.choose(choice)
+        state = self.receiver.receive_game_state()
+        return Observation(state)
+
+    def click(self, x: int, y: int, left: bool = True) -> Observation:
+        # self.receiver.empty_fifo()
+        self.sender.send_click(x,y, left=left)
+        state = self.receiver.receive_game_state()
+        return Observation(state)
+
+    def end(self) -> Observation:
+        # self.receiver.empty_fifo()
+        self.sender.send_end()
+        state = self.receiver.receive_game_state()
+        return Observation(state)
+
+    def potion(self, action, slot, target) -> Observation:
+        # self.receiver.empty_fifo()
+        self.sender.send_potion(action, slot, target)
+        state = self.receiver.receive_game_state()
+        return Observation(state)
+
+    def proceed(self) -> Observation:
+        # self.receiver.empty_fifo()
+        self.sender.send_proceed()
+        state = self.receiver.receive_game_state()
+        return Observation(state)
+
+    def start(self, player_class: str, ascension: int, seed: int) -> Observation:
+        # self.receiver.empty_fifo()
+        self.sender.send_start(player_class, ascension, seed)
+        state = self.receiver.receive_game_state()
+        return Observation(state)
+
+    def state(self) -> Observation:
+        """
+        Get the JSON representation of the current game state, regardless of whether or
+        not the game is "stable." This method is valid in all game states.
+        """
+
+        # self.receiver.empty_fifo()
+        self.sender.send_state()
+        state = self.receiver.receive_game_state()
+        return Observation(state)
+
+    def wait(self, frames: int) -> Observation:
+        # self.receiver.empty_fifo()
+        self.sender.send_wait(frames)
+        state = self.receiver.receive_game_state()
+        return Observation(state)
--- gym_sts/communication/receiver.py
+++ gym_sts/communication/receiver.py
@@ -1,19 +1,42 @@
+import fcntl
 import json
+import os
+import time
 
 
 class Receiver:
     def __init__(self, fn):
         self.fh = open(fn, "r")
 
-    def receive_game_state(self):
+        # Reading the pipe does not block if there are no contents
+        flag = fcntl.fcntl(self.fh, fcntl.F_GETFD)
+        fcntl.fcntl(self.fh, fcntl.F_SETFL, flag | os.O_NONBLOCK)
+
+    def empty_fifo(self) -> None:
+        """
+        Read and discard all pipe content.
+
+        Typically the caller would do this to ensure that the next message on the fifo
+        corresponds to the result of the next action sent to the game.
+        """
+
+        self.fh.read()
+
+    def receive_game_state(self) -> dict:
         """
         Continues reading game state until the game is waiting for action from
         the agent
         """
         print("Waiting to receive game state...")
         for i in range(1000):
-            state = json.loads(self.fh.readline())
-            if state["ready_for_command"]:
-                return state
+            message = self.fh.readline()
+            if len(message) > 0:
+                state = json.loads(message)
+                if state["ready_for_command"]:
+                    return state
+
+            # Try again in 5 milliseconds
+            time.sleep(0.005)
+
 
         raise Exception("Waited 1000 messages for game state to be ready for command, but it didn't happen.")
