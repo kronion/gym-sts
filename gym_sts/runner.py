53af2ddda46f3c97de3ce1e1d4dff742c2bc1095
Zeus Kronion <kronion@users.noreply.github.com>
2022-08-16 23:38:49 -0500

    Update runner.py

--- gym_sts/runner.py
+++ gym_sts/runner.py
@@ -6,10 +6,13 @@ from gym_sts.spaces.observations import ObservationError
 
 def main():
     parser = argparse.ArgumentParser()
+    parser.add_argument("lib_dir")
+    parser.add_argument("mods_dir")
+    parser.add_argument("out_dir")
     parser.add_argument("--headless", action="store_true")
     args = parser.parse_args()
 
-    env = SlayTheSpireGymEnv("out", headless=args.headless)
+    env = SlayTheSpireGymEnv(args.lib_dir, args.mods_dir, args.out_dir, headless=args.headless)
     env.reset()
 
     while True:
