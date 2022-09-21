940dda29de17f10482d90e7492f1221f99f3c88c
Vlad Firoiu <vladfi2@gmail.com>
2022-09-21 08:21:21 +0000

    Configure receiver timeout.

--- gym_sts/communication/receiver.py
+++ gym_sts/communication/receiver.py
@@ -5,12 +5,16 @@ import time
 
 
 class Receiver:
-    def __init__(self, fn):
+    def __init__(self, fn, timeout: float = 50):
         self.fh = open(fn, "r")
 
         # Reading the pipe does not block if there are no contents
         flag = fcntl.fcntl(self.fh, fcntl.F_GETFD)
         fcntl.fcntl(self.fh, fcntl.F_SETFL, flag | os.O_NONBLOCK)
+
+        self.timeout = timeout
+        self.sleep_time = 0.05
+        self.num_steps = int(timeout / self.sleep_time)
 
     def empty_fifo(self) -> None:
         """
@@ -28,17 +32,17 @@ class Receiver:
         the agent
         """
         print("Waiting to receive game state...")
-        for i in range(1000):
+
+        for _ in range(self.num_steps):
             message = self.fh.readline()
             if len(message) > 0:
                 state = json.loads(message)
                 if state["ready_for_command"]:
                     return state
 
-            # Try again in 50 milliseconds
-            time.sleep(0.05)
+            time.sleep(self.sleep_time)
 
         raise Exception(
-            "Waited 1000 messages for game state to be ready for command, "
-            "but it didn't happen."
+            f"Waited {self.timeout} seconds for game state to be ready "
+            "for command, but it didn't happen."
         )
