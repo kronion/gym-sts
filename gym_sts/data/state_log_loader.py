ffbcd8ee76566238e5b31c07b0dd5004886b35ac
spdskatr <25078279+spdskatr@users.noreply.github.com>
2022-11-07 22:20:25 +0000

    Improve state log loader

--- gym_sts/data/state_log_loader.py
+++ gym_sts/data/state_log_loader.py
@@ -1,21 +1,51 @@
 import json
 from typing import TextIO
 
+import numpy as np
+from tqdm import tqdm
+
+from gym_sts.spaces.actions import ACTIONS
+from gym_sts.spaces.observations import Observation
+
+
+# TODO: Remove this after everything in the main obs space outputs numpy arrays
+def preprocess(d: dict):
+    for k in d.keys():
+        v = d[k]
+        if type(v) == dict:
+            preprocess(v)
+        elif type(v) == list:
+            d[k] = np.array(v)
+        elif type(v) == int:
+            pass
+        else:
+            print(type(v))
+            assert False
+    return d
+
 
 class StateLogLoader:
     def __init__(self):
-        self.data = []
+        self.state_data = []
+        self.step_data = {"state_before": [], "action": [], "state_after": []}
 
     def load_file(self, fh: TextIO):
         objs = json.load(fh)
 
-        # Discard first entry, as we don't know what came before it
-        # For each subsequent entry, format as (before, action, after)
-        for i in range(1, len(objs)):
-            self.data.append(
-                {
-                    "state_before": objs[i - 1]["state_after"],
-                    "action": objs[i]["action"],
-                    "state_after": objs[i]["state_after"],
-                }
-            )
+        action_strings = [act.to_command() for act in ACTIONS]
+
+        prev_obs = None
+
+        for obj in tqdm(objs):
+            cur_obs = Observation(obj["state_after"]).serialize()
+            preprocess(cur_obs)
+
+            self.state_data.append(cur_obs)
+
+            # Store step data
+            if prev_obs and obj["action"]:
+                self.step_data["state_before"].append(cur_obs)
+                self.step_data["action"].append(action_strings.index(obj["action"]))
+                self.step_data["state_after"].append(cur_obs)
+
+            prev_obs = cur_obs
