12826d192870b790876fc17a1d38640ccce8f065
spdskatr <25078279+spdskatr@users.noreply.github.com>
2023-02-02 20:31:40 +0000

    Add current HP to reward function

--- gym_sts/envs/single_combat.py
+++ gym_sts/envs/single_combat.py
@@ -45,7 +45,10 @@ def single_combat_value(obs: Observation) -> float:
     max_hp = sum(e.max_hp for e in obs.combat_state.enemies)
     enemy_hp = sum(e.current_hp for e in obs.combat_state.enemies)
 
-    if max_hp == 0:
-        return 100
+    self_hp = obs.persistent_state.hp
 
-    return ((max_hp - enemy_hp) / max_hp) * 100
+    if max_hp == 0:
+        enemy_hp = 1
+        max_hp = 1
+
+    return ((max_hp - enemy_hp) / max_hp) * 100 + self_hp
