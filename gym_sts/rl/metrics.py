caba2bdd5a8fafc29f85c183ee2967439808a25f
spdskatr <25078279+spdskatr@users.noreply.github.com>
2023-04-16 18:46:38 +0100

    Tweak metrics logging

--- gym_sts/rl/metrics.py
+++ gym_sts/rl/metrics.py
@@ -20,5 +20,18 @@ class StSCustomMetricCallbacks(DefaultCallbacks):
         **kwargs
     ):
         obs = Observation.deserialize(episode.last_raw_obs_for())
+
+        max_hp = sum(e.max_hp for e in obs.combat_state.enemies)
+        enemy_hp = sum(e.current_hp for e in obs.combat_state.enemies)
         self_hp = obs.persistent_state.hp
-        episode.custom_metrics["win_rate"] = 1 if self_hp > 0 else 0
+        self_max_hp = obs.persistent_state.max_hp
+
+        if self_hp == 0 or enemy_hp == 0:
+            episode.custom_metrics["win_rate"] = 1 if self_hp > 0 else 0
+
+        if enemy_hp == 0:
+            episode.custom_metrics["win_remaining_hp"] = self_hp / self_max_hp
+        else:
+            episode.custom_metrics["lose_enemy_hp"] = (
+                enemy_hp / max_hp if max_hp > 0 else 0
+            )
